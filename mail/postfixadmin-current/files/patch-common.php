acf4229657b5d6c1bc666ecbfb971cf5e33faaa7
Raivo Hool <631910+freultwah@users.noreply.github.com>
2026-08-23 13:37:56 +0000

    mail/postfixadmin-current: update to Smarty 5

--- mail/postfixadmin-current/files/patch-common.php
+++ mail/postfixadmin-current/files/patch-common.php
@@ -12,7 +12,7 @@
  /**
   * Postfix Admin
   *
-@@ -65,6 +59,33 @@
+@@ -65,6 +59,44 @@
  
  $incpath = dirname(__FILE__);
  
@@ -24,7 +24,18 @@
 +{
 +    $base = __DIR__;
 +
-+    // 1) Namespaced classes (e.g. model\Languages)
++    // 1) Smarty 5 uses PSR-4 classes rooted in lib/smarty/src/.
++    $smarty_prefix = 'Smarty\\';
++    if (strncmp($class, $smarty_prefix, strlen($smarty_prefix)) === 0) {
++        $relative = substr($class, strlen($smarty_prefix));
++        $file = $base . '/lib/smarty/src/' . str_replace('\\', '/', $relative) . '.php';
++        if (is_file($file)) {
++            require_once $file;
++            return;
++        }
++    }
++
++    // 2) Namespaced PostfixAdmin classes (e.g. model\Languages)
 +    if (strpos($class, '\\') !== false) {
 +        $file = $base . '/' . str_replace('\\', '/', $class) . '.php';
 +        if (is_file($file)) {
@@ -33,7 +44,7 @@
 +        }
 +    }
 +
-+    // 2) Legacy global classes stored in model/
++    // 3) Legacy global classes stored in model/
 +    $legacy = $base . '/model/' . $class . '.php';
 +    if (is_file($legacy)) {
 +        require_once $legacy;
@@ -46,12 +57,10 @@
  if (!is_file("$incpath/config.inc.php")) {
      die("config.inc.php is missing!");
  }
-@@ -107,7 +128,11 @@
+@@ -107,6 +139,9 @@
      if (!isset($PALANG)) {
          die("environment not setup correctly");
      }
-+    require_once(__DIR__  . '/lib/smarty/libs/Autoloader.php');
-     Smarty_Autoloader::register();
  }
  
 +require_once(__DIR__  . '/lib/pacrypt/src/Crypt.php');
